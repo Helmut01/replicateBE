0b432f5bbbdfa5a57d83ccadf549206e0da4b434
Detlew <detlewlabes@gmx.de>
2017-06-27 11:02:23 +0200

    Check scripts under version control

---
+++ Check_scipts/CheckCSV.R
@@ -0,0 +1,23 @@
+########################################
+# Scripts to check internal reference  #
+# data sets imported from CSV-files.   #
+########################################
+sets    <- 1:21
+set     <- sprintf("%02i", sets)
+path.in <- paste0(find.package("replicateBE"), "/extdata/") # example files
+### ABE ###
+for (j in seq_along(sets)) {
+  ABE(path.in=path.in, file="DS", set=set[j], ext="csv")
+}
+### method.A ###
+for (j in seq_along(sets)) {
+  method.A(path.in=path.in, file="DS", set=set[j], ext="csv", ola=TRUE, plot.bxp=TRUE)
+}
+### method.B (option=2) ###
+for (j in seq_along(sets)) {
+  method.B(path.in=path.in, file="DS", set=set[j], ext="csv", ola=TRUE)
+}
+### method.B (option=1) ###
+for (j in seq_along(sets)) {
+  method.B(path.in=path.in, file="DS", set=set[j], ext="csv", ola=TRUE, option=1)
+}
